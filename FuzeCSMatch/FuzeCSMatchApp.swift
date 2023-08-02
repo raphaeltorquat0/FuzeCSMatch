db48d4ed93a42ffebf8ef04a71705ef89c318a9f
Raphael Torquato <raphael.martini@live.com>
2023-08-02 17:14:13 -0300

    Initial Commit

---
+++ FuzeCSMatch/FuzeCSMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuzeCSMatchApp.swift
+//  FuzeCSMatch
+//
+//  Created by Raphael Buchler on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FuzeCSMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
